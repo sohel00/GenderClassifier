1a470c682fe8543bd6af544fad467b971fa25cf9
Sohel Dhengre <sohel@Sohels-MacBook-Air.local>
2017-08-16 01:26:14 +0530

    no message

---
+++ GenderClassifier.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+
+# [height,weight,shoe size]
+X = [[180,81,44],[177,40,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,39],[159,55,37],[171,75,42],[181,85,43]]
+Y = ['male','female','female','female','male','male','male','female','male','female','male']
+
+classify = tree.DecisionTreeClassifier()
+
+classify = classify.fit(X,Y)
+
+prediction = classify.predict([[140,75,45]])
+
+print prediction
